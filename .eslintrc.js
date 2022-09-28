09aab34b3b9207b5a91dd789c044c126b12328cf
Cornelius Ukena <cornelius@mostlyserious.io>
2022-09-28 13:32:22 -0500

    🚸 marker integration improvements

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     ],
     settings: {
         'html/indent': '+4',
-        'html/html-extensions': [ '.html', '.php' ],
+        'html/html-extensions': [ '.html', '.twig', '.php' ],
         'svelte3/ignore-styles': () => true
     },
     overrides: [
--- modules/Module.php
+++ modules/Module.php
@@ -34,6 +34,17 @@ class Module extends BaseModule
                         .button.button-prose:hover { text-decoration: none; }
                         .color-swatches { padding: 0 3px 3px 3px; }
                     ');
+
+                    if (getenv('ENVIRONMENT') === 'staging' && getenv('MARKERIO_DESTINATION')) {
+                        $view->registerJs(sprintf('
+                            window.markerConfig = {
+                                destination: "%s",
+                                source: "snippet"
+                            };
+                        ', getenv('MARKERIO_DESTINATION')));
+
+                        $view->registerJs('!function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);');
+                    }
                 }
             );
         }
