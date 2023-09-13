6ada62bda08796591d50d424d6d01155757dd94f
Cornelius Ukena <cornelius.ukena@mostlyserious.io>
2023-09-13 11:51:40 -0500

    🐛 Fix bootstrap:publish script

--- bootstrap/publish.sh
+++ bootstrap/publish.sh
@@ -4,8 +4,10 @@ rm -r bootstrap/project
 
 cp -r config/project bootstrap/project
 
-rm config/project
-rm config/license.key
+rm -r config/project
+rm -r config/license.key
+
+git checkout web/cpresources/.gitkeep
 
 echo ""
 echo "The bootstrap project config has been updated to the current state.\nYou may commit these changes."
